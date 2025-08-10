1a8c9f0286dc8128f3f509e63d5df941393f789d
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-08-10 18:38:52 +0530

    add

---
+++ Calc.py
@@ -0,0 +1,21 @@
+# Calaulator Operation in Python
+def additon(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+def exponentiation(a,b):
+    return a**b
+
+def modulus(a,b):
+    return a%b
+
+def floor_division(a,b):
+    return a//b
