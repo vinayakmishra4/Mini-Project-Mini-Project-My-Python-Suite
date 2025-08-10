4a64bf3f0a0ef329f7b134db097715acb72e8bc0
VINAYAK MISHRA <vmaugust8@gmail.com>
2025-08-10 19:37:33 +0530

    Update Pythonsutie.py

--- Pythonsutie.py
+++ Pythonsutie.py
@@ -1,1 +1,8 @@
 # Mini Project Python Suite
+import Calc as c
+def Calaculator():
+
+    a=int(input("Enter the First Number: "))
+    b=int(input("Enter the Second Number: "))
+    print("-"*15)
+    
